0147a9fa59fc5a6e6328a5fc73dcd6f25eec76cd
gregorymurad <greis003@fiu.edu>
2022-12-09 20:00:27 -0500

    fixing butterfly project page

--- pages/5_💕_Mariah_Carey.py
+++ pages/5_💕_Mariah_Carey.py
@@ -10,7 +10,6 @@ import pandas as pd
 import plotly.express as px
 
 
-
 st.set_page_config(layout="wide",page_icon="🦋",)
 
 page_bg_img = f"""
@@ -191,11 +190,11 @@ def generate_plot(tuples_freq_dist,title):
 st.header("Most Used Words Overall 🎤")
 all_lyrics = get_lyrics()
 all_lyrics_freq_dist = generate_frequency_distribution(all_lyrics)
-st.write(all_lyrics_freq_dist)
+
 word_length = st.slider('Select the minimum word length', 0, 12, 2)
 
 filtered_by_length = filter_by_length(all_lyrics_freq_dist,word_length)
-st.write(filtered_by_length)
+
 generate_plot(filtered_by_length,"Most Frequent Used Words")
 
 st.markdown("---")
